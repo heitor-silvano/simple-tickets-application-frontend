c81df1b9a72548464a98bd7249f073215f1bd605
Heitor Silvano <heitor.nunes182@hotmail.com>
2026-03-25 23:15:41 -0300

    wip: ticket card

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,9 +1,52 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const Tickets = () => {
+  const mockedTickets = [
+    {
+      eventName: "Evento teste",
+      attendeeName: "Heitorito",
+      attendeeEmail: "heitor.nunes182@hotmail.com",
+      ticketTypeTitle: "Comum",
+      status: "Teste",
+    },
+    {
+      eventName: "Evento teste",
+      attendeeName: "Heitorito",
+      attendeeEmail: "heitor.nunes182@hotmail.com",
+      ticketTypeTitle: "Comum",
+      status: "Teste",
+    },
+  ];
+
   return (
     <>
-      hello, tickets
+      <Card className="p-8">
+        <CardHeader>
+          <CardTitle>Meus Tickets</CardTitle>
+        </CardHeader>
+        <CardDescription>0 Tickets encontrados</CardDescription>
+        {mockedTickets.map((item, index) => (
+          <div className="flex flex-row">
+            <Card key={index} className="flex flex-col w-min">
+              <CardContent>{item.eventName}</CardContent>
+              <CardContent>{item.attendeeName}</CardContent>
+              <CardContent>{item.attendeeEmail}</CardContent>
+              <div className="flex flex-row">
+                <CardContent>{item.ticketTypeTitle}</CardContent>
+                <CardContent>{item.status}</CardContent>
+              </div>
+            </Card>
+          </div>
+        ))}
+      </Card>
     </>
-  )
-}
+  );
+};
 
-export default Tickets
+export default Tickets;
