62365ce5c7e5c9fa9290689451533c0654d3181d
Heitor Silvano <heitor.nunes182@hotmail.com>
2026-03-30 22:49:18 -0300

    wip: ticket page with data from backend

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -18,38 +18,40 @@ import {
 } from "@/components/ui/sheet";
 import { QRCodeSVG } from "qrcode.react";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 const Tickets = () => {
-  const mockedTickets = [
+  const [tickets, setTickets] = useState<
     {
-      eventName: "Neon Pulse Festival",
-      attendeeName: "Heitorito",
-      attendeeEmail: "heitor.nunes182@hotmail.com",
-      ticketTypeTitle: "Comum",
-      status: "Teste",
-      hashId: "4aa4667970ba4c9f9a11fd13fe8c1a71518de318",
-      location:
-        "Av. das Nações Unidas, 17955 - Vila Almeida, São Paulo - SP, 04795-100",
-      eventImage: "/NeonSign.jpg",
-      createdAt: new Date(),
-      eventStartDate: new Date("2026-11-22 18:00"),
-      eventEndDate: new Date("2026-11-22 19:30"),
-    },
-    {
-      eventName: "Code & Coffee Summit",
-      attendeeName: "Heitorito",
-      attendeeEmail: "heitor.nunes182@hotmail.com",
-      ticketTypeTitle: "Comum",
-      status: "Teste",
-      hashId: "e208cf696247f0a4aa3e4947d6c1d09c56492362",
-      location:
-        "Av. das Nações Unidas, 17955 - Vila Almeida, São Paulo - SP, 04795-100",
-      eventImage: "/CannabisCoffeeShopAmsterdam.jpg",
-      createdAt: new Date(),
-      eventStartDate: new Date("2026-11-12 15:00"),
-      eventEndDate: new Date("2026-10-22 17:30"),
-    },
-  ];
+      eventTitle?: string | null;
+      attendeeName?: string | null;
+      attendeeEmail: string;
+      ticketTypeTitle?: string | null;
+      status: string;
+      hashId: string | null;
+      location: string | null;
+      eventImage?: string | null;
+      createdAt: Date;
+      eventStartDate?: Date | null;
+      eventEndDate?: Date | null;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/tickets");
+
+        const data = await response.json();
+
+        setTickets(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchTickets();
+  }, []);
 
   return (
     <>
@@ -65,7 +67,7 @@ const Tickets = () => {
           <Button className="bg-chart-2">Remover filtro</Button>
         </div>
         <Card className="flex flex-row bg-muted px-4">
-          {mockedTickets.map((item, index) => (
+          {tickets.map((ticket, index) => (
             <Card
               key={index}
               className="flex flex-col py-0 w-min hover:bg-secondary hover:cursor-pointer"
@@ -75,61 +77,67 @@ const Tickets = () => {
                   <div className="flex flex-col items-start hover:cursor-pointer">
                     <div className="relative w-full h-30 aspect-video">
                       <Image
-                        src={item.eventImage}
+                        src={ticket.eventImage ?? ""}
                         fill
-                        alt={item.eventName}
+                        alt={ticket.eventTitle ?? ""}
                         className="object-cover"
                       />
                     </div>
                     <div className="flex flex-col items-start py-4">
-                      <CardContent>{item.eventName}</CardContent>
-                      <CardContent>{item.attendeeName}</CardContent>
-                      <CardContent>{item.attendeeEmail}</CardContent>
+                      <CardContent>{ticket.eventTitle}</CardContent>
+                      <CardContent>{ticket.attendeeName}</CardContent>
+                      <CardContent>{ticket.attendeeEmail}</CardContent>
                       <div className="flex flex-row">
-                        <CardContent>{item.ticketTypeTitle}</CardContent>
-                        <CardContent>{item.status}</CardContent>
+                        <CardContent>{ticket.ticketTypeTitle}</CardContent>
+                        <CardContent>{ticket.status}</CardContent>
                       </div>
                     </div>
                   </div>
                 </SheetTrigger>
                 <SheetContent>
                   <div className="p-5">
-                    <SheetTitle>Ticket - {item.eventName}</SheetTitle>
+                    <SheetTitle>Ticket - {ticket.eventTitle}</SheetTitle>
                   </div>
                   <div className="relative w-full h-50 aspect-video">
                     <Image
-                      src={item.eventImage}
+                      src={ticket.eventImage ?? ""}
                       fill
-                      alt={item.eventName}
+                      alt={ticket.eventTitle ?? ""}
                       className="object-cover"
                     />
                   </div>
                   <SheetHeader>
                     <SheetDescription>
                       Comprado em{" "}
-                      {Intl.DateTimeFormat("pt-br").format(item.createdAt)}
+                      {/* {Intl.DateTimeFormat("pt-br").format(item.createdAt)} */}
                     </SheetDescription>
                     <SheetDescription>
                       Nota: não exponha seu QR Code, nem mesmo parcialmente.
-                      <QRCodeSVG
-                        value={item.hashId}
-                        className="w-full h-auto px-8 py-4"
-                      />
-                      {item.hashId}
+                      {ticket.hashId && (
+                        <QRCodeSVG
+                          value={ticket.hashId}
+                          className="w-full h-auto px-8 py-4"
+                        />
+                      )}
+                      {ticket.hashId}
                     </SheetDescription>
                     <SheetTitle>Data</SheetTitle>
-                    <SheetDescription>
-                      {Intl.DateTimeFormat("pt-br", {
-                        weekday: "long",
-                        year: "numeric",
-                        month: "2-digit",
-                        day: "2-digit",
-                        hour: "2-digit",
-                        minute: "2-digit",
-                      }).format(item.eventStartDate)}
-                    </SheetDescription>
+                    {
+                      <SheetDescription>
+                        {ticket.eventStartDate
+                          ? Intl.DateTimeFormat("pt-br", {
+                              weekday: "long",
+                              year: "numeric",
+                              month: "2-digit",
+                              day: "2-digit",
+                              hour: "2-digit",
+                              minute: "2-digit",
+                            }).format(new Date(ticket.eventStartDate))
+                          : "--"}
+                      </SheetDescription>
+                    }
                     <SheetTitle>Local</SheetTitle>
-                    <SheetDescription>{item.location}</SheetDescription>
+                    <SheetDescription>{ticket.location}</SheetDescription>
                   </SheetHeader>
                 </SheetContent>
               </Sheet>
